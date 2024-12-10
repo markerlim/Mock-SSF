02c6f44089f0df0e978400ab955ff31e66f9dc41
Marcus Lim <markerlim15@gmail.com>
2024-12-10 11:11:44 +0800

    adding spring

---
+++ src/test/java/com/assessment/ssf/MOCK_SSF/MockSsfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.ssf.MOCK_SSF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockSsfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
